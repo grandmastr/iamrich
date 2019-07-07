c1dd04258b3b62962960d622961b5cd30872347e
Israel Akintunde <israelakintunde005@gmail.com>
2019-07-07 12:56:08 +0100

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,19 @@ import 'package:flutter/material.dart';
 //the main function is the starting point in our app
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('I am rich'),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        backgroundColor: Colors.blueGrey,
-        body: Image(
-          image: NetworkImage("https://d2d8wwwkmhfcva.cloudfront.net/600x/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_3ae23d47-7d52-4c5d-b18e-ffd22ce0d773.jpeg"),
-        )
-      ),
-    ),
-  );
+  	runApp(
+  		MaterialApp(
+   		   	home: Scaffold(
+   		       	appBar: AppBar(
+   		       	title: Text('I am rich'),
+            	backgroundColor: Colors.blueGrey[900],
+          	),
+          	backgroundColor: Colors.blueGrey,
+          	body: Image(
+            	image: NetworkImage(
+            	    "https://d2d8wwwkmhfcva.cloudfront.net/600x/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_3ae23d47-7d52-4c5d-b18e-ffd22ce0d773.jpeg"),
+          	)),
+   		),
+		
+  	);
 }
